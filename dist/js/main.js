b7e8292eaf08282234be70d4767b61d00b2ac743
helevents <fawnliu2333@qq.com>
2016-11-16 11:30:14 +0800

    cahnge

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,99 +1,110 @@
 const canvas = $("#canvas");
 const ctx = canvas.getContext("2d");
 let imgUrls = ['../images/1.jpg', '../images/2.jpg'];
-let touchFlag = false;
 let moveFlag = false;
 //游戏主体部分
 drawStage();
 
 let currentPlace = [];
-// currentPlace[0] = newImg(160, 40, imgUrls[1]);
-// currentPlace[1] = newImg(120, 80, imgUrls[1]);
+currentPlace[0] = newImg(160, 40, imgUrls[1]);
+currentPlace[1] = newImg(120, 80, imgUrls[1]);
 currentPlace[2] = newImg(160, 80, imgUrls[0]);
 currentPlace[3] = newImg(200, 80, imgUrls[1]);
-// currentPlace[4] = newImg(160, 120, imgUrls[1]);
+currentPlace[4] = newImg(160, 120, imgUrls[1]);
 
-canvas.addEventListener('touchstart', function(e0) {
-    moveFlag = true;
-    touchmove = true;
-
-    var e0 = e0 || window.event;
-
-    let start = {
-        x: e0.touches[0].clientX,
-        y: e0.touches[0].clientY
-    };
-
-    //将点击屏幕上的图片时, 将当前图片的位置设为40的倍数
-    let startInt = {
-        x: parseInt((start.x - this.offsetLeft) / 40) * 40,
-        y: parseInt((start.y - this.offsetTop) / 40) * 40
-    };
+(function () {
+    let start = { x: 0, y: 0 };
+    let startInt = { x: 0, y: 0 };
     let imgPlace = { x: startInt.x, y: startInt.y };
 
+    canvas.addEventListener('touchstart', function(e0) {
+        var e0 = e0 || window.event;
+
+        start = {
+            x: e0.touches[0].clientX,
+            y: e0.touches[0].clientY
+        };
+
+        //将点击屏幕上的图片时, 将当前图片的位置设为40的倍数
+        startInt = {
+            x: parseInt((start.x - this.offsetLeft) / 40) * 40,
+            y: parseInt((start.y - this.offsetTop) / 40) * 40
+        };
+    });
+
     canvas.addEventListener('touchmove', function(e) {
-        touchFlag = false;
         var e = e || window.event;
 
-        if (moveFlag) {
-            let mouse = {
-                x: e.touches[0].clientX,
-                y: e.touches[0].clientY
-            };
-                
-            //比较当前鼠标和图片开始时的位置
-            if (Math.abs(mouse.y - start.y) < 20) {
-                //鼠标往 右/下 移动时
+        let mouse = {
+            x: e.touches[0].clientX,
+            y: e.touches[0].clientY
+        };
+        //比较当前鼠标和图片开始时的位置
+        if (Math.abs(mouse.y - start.y) < 10) {
+            //鼠标向右移动
+            if (mouse.x - start.x >= 0) {
+                //判断鼠标 向右 移动的距离
                 if (mouse.x - start.x >= 20) {
                     imgPlace = {
                         x: startInt.x + 40,
                         y: startInt.y
                     };        
-                    //鼠标往 左/上移动时 
-                } else if (start.x - mouse.x > 20) {
+                } else {
+                    imgPlaceStay();
+                }
+                //鼠标向左移动
+            } else {
+                if (mouse.x - start.x <= -20) {
                     imgPlace = {
                         x: startInt.x - 40,
                         y: startInt.y
                     };
-                    //移动距离不满足要求的情况
                 } else {
-                    imgPlace = { x: startInt.x, y: startInt.y };
+                    imgPlaceStay();
                 }
             } 
-            
-            if (Math.abs(mouse.x - start.x) < 20) {
+
+        } 
+        
+        if (Math.abs(mouse.x - start.x) < 10) {
+            if (mouse.y - start.y >= 0) {
                 if (mouse.y - start.y >= 20) {
                     imgPlace = {
                         x: startInt.x,
                         y: startInt.y + 40
                     }                    
-                } else if (start.y - mouse.y > 20) {
+                } else {
+                    imgPlaceStay();
+                }
+            } else {
+                if (start.y - mouse.y >= 20) {
                     imgPlace = {
                         x: startInt.x,
                         y: startInt.y - 40
-                    }                    
+                    };
                 } else {
-                    imgPlace = {x: startInt.x, y: startInt.y};
+                    imgPlaceStay();
                 }
             }
-
-            newImg(imgPlace.x, imgPlace.y, imgUrls[0]);
-
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            drawStage();  
-            // newImg(160, 40, imgUrls[1]);
-            // newImg(120, 80, imgUrls[1]);
-            // newImg(200, 80, imgUrls[1]);
-            // newImg(160, 120, imgUrls[1]);
+        }
         
-            canvas.addEventListener('touchend', function (e) {
-                moveFlag = false;
-                startInt.x = imgPlace.x;
-                startInt.y = imgPlace.y;
-            });
+        //交换图片
+        newImg(startInt.x, startInt.y, imgUrls[1]);
+        newImg(imgPlace.x, imgPlace.y, imgUrls[0]);
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawStage();
+    });
+    canvas.addEventListener('touchend', function (e) {
+        startInt = {
+            x: imgPlace.x,
+            y: imgPlace.y
         }
     });
-});
+    function imgPlaceStay() {
+        imgPlace = { x: startInt.x, y: startInt.y };        
+    }    
+})();
 
 function newImg(a, b, src) {
     var img = new Image();
@@ -101,7 +112,10 @@ function newImg(a, b, src) {
     img.onload = function() {
         ctx.drawImage(img, a, b, 40, 40);
     };
-    return img;
+    return {
+        x: a, 
+        y: b
+    };
 }
 
 function drawStage() {
