11a711b2af45b7271a8b489200baf01ae153ef8e
helevents <fawnliu2333@qq.com>
2016-11-17 21:43:55 +0800

    cahnge

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,8 +1,3 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-let imgUrls = ['../images/1.jpg', '../images/2.jpg', '../images/3.jpg'];
-let moveFlag = false;
-
 $(window).on('scroll.elasticity', function (e){
     e.preventDefault();
 }).on('touchmove.elasticity', function(e){
@@ -10,24 +5,42 @@ $(window).on('scroll.elasticity', function (e){
 });
 
 $(document).ready(() => {
-    if (window.innerHeight > 568) {
-        $('body').height = window.innerHeight;
-    } 
-    if (window.innerWidth > 320) {
-        $('body').width = window.innerHeight;
-    }
+    
     const pub = {
         canvas: document.getElementById('canvas'),
         ctx: document.getElementById('canvas').getContext('2d'),
         imgWidth: 40,
         imgHeight: 40,
         halfWidth: 20,
-        allImgs: [],
-        allImgsUrl: ['../images/1.jpg', '../images/2.jpg', '../images/3.jpg', '../images/4.jpg', '../images/5.jpg', '../images/6.jpg'],
+        //第一行图片的数量
+        xNum: 7,
+        //第一列图片的数量
+        yNum: 7,
+        allImgs: document.querySelector('.allimg').children,
         gameOver () {
             alert('gameover');
         }
     }
+
+    let pubdata = {
+        moveFlag : false,
+        //移动位置不合理时 
+        imgPlaceStay () {
+            imgPlace = {
+                x: startInt.x,
+                y: startInt.y
+            };
+        }
+    };
+
+    //记录触发touchstart事件的位置
+    let start = {};
+    //将 触发touchstart事件的位置 设置为imgWidth的整数倍
+    let startInt = {};
+    //点击的图片 下一刻要去的地方
+    let imgPlace = {};
+    //存放页面上所有的图片信息
+    let matrix = [];
 
     class Stage {
         constructor () {
@@ -39,36 +52,89 @@ $(document).ready(() => {
         //每次消除小动物之后 刷新页面
         refresh () {
             this.ctx.clearRect(0, 0, this.width, this.height);
-
-
         }
         //游戏开始时 填充图片
+        drawNewStage () {
+            for (let i = 0; i <= pub.xNum; i++) {
+                matrix[i] = new Array();
+                for (let j = 0; j <= pub.yNum; j++) {
+                    let len = pub.allImgs.length;
+                    let index = Math.round(Math.random()*(len-1) + 0);
+                    let animal = new Animal(pub.ctx, i*pub.imgWidth, j*pub.imgHeight, pub.allImgs[index]);
+
+                    matrix[i][j] = animal;
+                    animal.paint();
+                }
+            }
+
+            //动画部分
+            // for (let i = 0; i <= 0; i++) {
+            //     for (let j = 0; j <= 0; j++) {
+            //         matrix[i][j].setY(matrix[i][j].getY() - (240 - j*pub.imgHeight));
+            //         console.log(matrix[i]);
+            //         var timer = setInterval(function () {
+            //             matrix[i][j].fall();
+            //             if (matrix[i][j].getY() > i*pub.imgHeight) {
+            //                 clearInterval(timer);
+            //             }
+            //         }, 10);
+            //     }
+            // }
+        }
+
+        //对 存入matrix 的图片进行重绘
         drawStage () {
-            this.ctx.strokeStyle = "#797979";
-            this.ctx.strokeRect(0, 0, 280, 280);
-            for (var i = 1; i <= 6; i++) {
-                this.ctx.beginPath();
-                this.ctx.lineWidth = "2";
-                this.ctx.strokeStyle = "#797979";
-                this.ctx.moveTo(0, 0 + 40 * i);
-                this.ctx.lineTo(280, 0 + 40 * i);
-                this.ctx.stroke();
-            }
-
-            for (var i = 1; i <= 6; i++) {
-                this.ctx.beginPath();
-                this.ctx.lineWidth = "2";
-                this.ctx.strokeStyle = "#797979";
-                this.ctx.moveTo(40 * i, 0);
-                this.ctx.lineTo(40 * i, 280);
-                this.ctx.stroke();
+            for (let i = 0; i <= pub.xNum; i++) {
+                for (let j = 0; j < pub.yNum; j++) {
+                    matrix[i][j].paint();
+                }
             }
         }
 
-        run () {
+        //判断是否达到 可消 的条件
+        isDissloved () {
+            let repeatImg = [];
+            for (let i = 0; i < pub.xNum - 2; i++) {
+                repeatImg[i] = new Array(); 
+                for (let j = 0; j < pub.yNum - 2; j++) {
 
+                    // function count(i) {
+                    //     for (let z = 0; z <= i; z++) {
+                    //         if (matrix[i][j+z].img == matrix[i][j+z+1].img) 
+                    //             continue;
+                    //     }
+                    //     if (z == i) {
+                    //         return true;
+                    //     }
+                    // }
+                    if (matrix[i][j].img == matrix[i][j+1].img && matrix[i][j+1].img == matrix[i][j+2].img) {
+                        console.log('yyy');
+                        console.log(i, j);
+                        
+                        console.log(matrix[i][j]);
+                        for (let z = 0; z <= 2; z++) {
+                            matrix[i][j+z].refresh();    
+                        }
+                            repeatImg[i][j] = matrix[i][j];
+                        
+                    }
+                    if (matrix[i][j].img == matrix[i+1][j].img && matrix[i+1][j].img == matrix[i+2][j].img) {
+                        console.log('xxx');
+                        console.log(i, j);
+                        
+                        console.log(matrix[i][j]);
+                        repeatImg[i][j] = matrix[i][j];
+
+                        for (let z = 0; z <= 2; z++) {
+                            matrix[i+z][j].refresh();    
+                        }
+                            repeatImg[i][j] = matrix[i][j];
+                    }
+                }
+            }   
+            console.log(repeatImg);
+            return repeatImg;
         }
-
     }
 
     class Animal {
@@ -79,84 +145,53 @@ $(document).ready(() => {
             this.img = img;
         }
 
-        getx () {
-            return this.x;
+        paint () {
+            this.ctx.drawImage(this.img, this.x, this.y, pub.imgWidth, pub.imgHeight);
         }
-
-        gety () {
-            return this.y;
-        }
-
-        // paint () {
-        //     this.ctx.drawImage(this.img, this.x, this.y, pub.imgWidth, pub.imgHeight);
-        // }
 
         fall () {
             this.y += .5;
-
             stage.refresh();
-            stage.drawStage();
+            this.paint();
         }
 
         //满足消去条件后 图片的变化
         eliminated () {
-
+            //不重绘即可
+            //此处还应该有一些动画
         } 
-    }
 
-
-    let pubdata = {
-        //记录触发touchstart事件的位置
-        start: {},
-        //将 触发touchstart事件的位置 设置为imgWidth的整数倍
-        startInt: {},
-        //点击的图片 下一刻要去的地方
-        imgPlace: {},
-        //移动位置不合理时 
-        imgPlaceStay () {
-            pubdata.imgPlace = {
-                x: pubdata.startInt.x,
-                y: pubdata.startInt.y
-            };
+        refresh () {
+            this.ctx.clearRect(this.x, this.y, pub.imgWidth, pub.imgHeight);
+            this.cxt.beginPath(); 
+            this.cxt.fillStyle="#000";/*设置填充颜色*/ 
+            this.cxt.fillRect(this.x, this.y, 40, 40);
+            // this.ctx.fillstyle = '#000';
+            // this.cxt.strokeRect(); 
         }
-    };
+    }
 
     let stage = new Stage();
 
-    pub.allImgsUrl.forEach( function(element, index) {
-        // let img = new Image();
-        // img.src = element;
-
-        // pub.allImgs[index] = img;
-        // let animal = new Animal(pub.ctx, 160, 80+index*40, img);
-        // img.onload = function () {
-        //     animal.paint();
-        // }
-        newImg(160, 80+index*40, element);
-    });
-
-    pub.allImgsUrl.forEach( function(element, index) {
-        newImg(index*40, 80, element);
-    });
-
-    stage.drawStage();
-
+    stage.drawNewStage();
+    stage.isDissloved();
 
     pub.canvas.addEventListener('touchstart', function(e) {
         var e = e || window.event;
 
-        pubdata.start = {
+        start = {
             x: e.touches[0].clientX,
             y: e.touches[0].clientY
         };
 
-        pubdata.startInt = {
-            x: parseInt((pubdata.start.x - this.offsetLeft) / pub.imgWidth) * pub.imgWidth,
-            y: parseInt((pubdata.start.y - this.offsetTop) / pub.imgWidth) * pub.imgWidth
+        startInt = {
+            x: parseInt((start.x - this.offsetLeft) / pub.imgWidth) * pub.imgWidth,
+            y: parseInt((start.y - this.offsetTop) / pub.imgWidth) * pub.imgWidth
         };
     });
 
     pub.canvas.addEventListener('touchmove', function(e) {
+        pubdata.moveFlag = true;
         var e = e || window.event;
 
         let mouse = {
@@ -164,90 +199,69 @@ $(document).ready(() => {
             y: e.touches[0].clientY
         };
         //手指拖动图片向 水平 方向运动
-        if (Math.abs(mouse.y - pubdata.start.y) < 10) {
+        if (Math.abs(mouse.y - start.y) <= Math.abs(mouse.x - start.x)) {
             //鼠标向右移动
-            if (mouse.x - pubdata.start.x >= 0) {
+            if (mouse.x - start.x >= 0) {
                 //如果鼠标 向右 拖动图片移动的距离 超过 图片的halfwidth
-                if (mouse.x - pubdata.start.x >= pub.halfWidth) {
-                    pubdata.imgPlace = {
-                        x: pubdata.startInt.x + pub.imgWidth,
-                        y: pubdata.startInt.y
+                if (mouse.x - start.x >= pub.halfWidth) {
+                    imgPlace = {
+                        x: startInt.x + pub.imgWidth,
+                        y: startInt.y
                     };        
                 } else {
                     pubdata.imgPlaceStay();
                 }
                 //鼠标向左移动
             } else {
-                if (mouse.x - pubdata.start.x <= -pub.halfWidth) {
-                    pubdata.imgPlace = {
-                        x: pubdata.startInt.x - pub.imgWidth,
-                        y: pubdata.startInt.y
+                if (mouse.x - start.x <= -pub.halfWidth) {
+                    imgPlace = {
+                        x: startInt.x - pub.imgWidth,
+                        y: startInt.y
                     };
                 } else {
                     pubdata.imgPlaceStay();
                 }
             } 
-        } 
-        //手指拖动图片向 竖直 方向运动
-        if (Math.abs(mouse.x - pubdata.start.x) < 10) {
+            //拖动图片向竖直方向运动
+        } else {
             //鼠标向下移动
-            if (mouse.y - pubdata.start.y >= 0) {
+            if (mouse.y - start.y >= 0) {
                 //如果鼠标 向下 拖动图片移动的距离 超过 图片的halfwidth
-                if (mouse.y - pubdata.start.y >= pub.halfWidth) {
-                    pubdata.imgPlace = {
-                        x: pubdata.startInt.x,
-                        y: pubdata.startInt.y + pub.imgWidth
+                if (mouse.y - start.y >= pub.halfWidth) {
+                    imgPlace = {
+                        x: startInt.x,
+                        y: startInt.y + pub.imgWidth
                     }                    
                 } else {
                     pubdata.imgPlaceStay();
                 }
             } else {
-                if (pubdata.start.y - mouse.y >= pub.halfWidth) {
-                    pubdata.imgPlace = {
-                        x: pubdata.startInt.x,
-                        y: pubdata.startInt.y - pub.imgWidth
+                if (start.y - mouse.y >= pub.halfWidth) {
+                    imgPlace = {
+                        x: startInt.x,
+                        y: startInt.y - pub.imgWidth
                     };
                 } else {
                     pubdata.imgPlaceStay();
                 }
             }
         }
-        
-        newImg(pubdata.startInt.x, pubdata.startInt.y, pub.allImgsUrl[0]);
-        newImg(pubdata.imgPlace.x, pubdata.imgPlace.y, pub.allImgsUrl[1])
-
-        stage.refresh();
-        stage.drawStage();
     });
 
     pub.canvas.addEventListener('touchend', function (e) {
-        pubdata.startInt = {
-            x: pubdata.imgPlace.x,
-            y: pubdata.imgPlace.y
-        }
+        if (pubdata.moveFlag) {
+            let temp = matrix[startInt.x / 40][startInt.y / 40].img;
+            matrix[startInt.x / 40][startInt.y / 40].img = matrix[imgPlace.x/40][imgPlace.y/40].img;
+            matrix[imgPlace.x/40][imgPlace.y/40].img = temp;
+            //将 matrix 里面的图片重绘
+            stage.drawStage();
+
+            startInt = {
+                x: imgPlace.x,
+                y: imgPlace.y
+            }    
+        } 
+
+        pubdata.moveFlag = false;
     });
-
-    function newImg(a, b, src) {
-    var img = new Image();
-    img.src = src;
-    img.onload = function() {
-        pub.ctx.drawImage(img, a, b, 40, 40);
-    };
-    return {
-        x: a, 
-        y: b
-    };
-
-}
-
 });
-
-
-
-// function $(selector) {
-//     if (document.querySelectorAll(selector).length === 1) {
-//         return document.querySelector(selector);
-//     } else {
-//         return document.querySelectorAll(selector);
-//     }
-// }
