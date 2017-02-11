f7d813f36119489d868d48473f2e1b1cc7a5bbbf
helevents <fawnliu2333@gmail.com>
2017-02-11 15:00:44 +0800

    webpack

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,9 @@ const imagemin = require('gulp-imagemin');
 const LessAutoprefix = require('less-plugin-autoprefix');
 const browserSync = require('browser-sync').create();
 const autoprefix = new LessAutoprefix({ browsers: ['last 2 versions'] });
-
-gulp.task('babelEs6', () => {
+var uglify = require('gulp-uglify');
+ 
+gulp.task('compile', () => {
     gulp.src('./src/js/*.js')
         .pipe(babel({
             presets: ['es2015']
@@ -19,6 +20,12 @@ gulp.task('imagemin', () =>
         .pipe(gulp.dest('./dist/images'))
 );
 
+gulp.task('jsmin', function () {
+    gulp.src('dist/js/*.js') 
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
 gulp.task('watch', () => {
     browserSync.init({
         server: {
@@ -26,7 +33,7 @@ gulp.task('watch', () => {
         }
     });
     gulp.watch('./src/images/*', ['imagemin']);
-    gulp.watch('./src/js/*.js', ['babelEs6']);
+    gulp.watch('./src/js/*.js', ['compile']);
     gulp.watch('dist/**/*').on('change', browserSync.reload);
     gulp.watch('dist/index.html').on('change', browserSync.reload);
 });
